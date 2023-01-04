7ca2908b4f35013046cf582ce0412f0021a09249
Roman Govtvyan <roman.govtvyan@uco.co.il>
2023-01-04 16:38:22 +0200

    add dictionary and words list. delete unused entity

---
+++ database/migrations/2023_01_04_142547_create_words_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('dictionary_id');
+            $table->string('word');
+            $table->text('api_response')->nullable();
+            $table->unsignedTinyInteger('count_response_attempts')->default(0);
+            $table->foreign('dictionary_id')->references('id')->on('dictionaries');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
